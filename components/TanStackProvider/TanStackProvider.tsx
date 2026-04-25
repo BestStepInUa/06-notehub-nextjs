1f690c6eaca42c1f4e7689dcc48348ef0095dbbf
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-04-25 20:10:09 +0300

    upd provider

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 
 function makeQueryClient() {
